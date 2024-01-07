3e575c9631a15ccbd6b6fa7601b05cbae4f9572f
Mykola <myksolarikgmail.com>
2024-01-07 21:17:44 +0200

    Add HostServerSendDataToClientTest

--- Chatik/tests/HostTest.cpp
+++ Chatik/tests/HostTest.cpp
@@ -2,8 +2,6 @@
 #include "../src/SocketUtil.h"
 #include "UnitTest++/UnitTest++.h"
 
-static std::atomic_bool onDataArrived;
-
 SUITE(HostTest)
 {
   TEST(HostServerStartStopTest)
@@ -34,23 +32,21 @@ SUITE(HostTest)
 
   TEST(HostClientSendDataToServerTest)
   {
-    constexpr char expectedData[] = "Hello, world!";
     char actualData[100] = {};
 
-    onDataArrived = false;
+    std::atomic_bool onServerDataArrivedTrigger(false);
 
-    auto onDataReceivedCallback =
+    auto onServerDataReceivedCallback =
       [&](const char* data,
           int dataLen,
           const Chatik::SocketAddress& fromAddress) {
         assert(data && dataLen > 0);
         memcpy(actualData, data, dataLen);
-
-        onDataArrived = true;
+        onServerDataArrivedTrigger = true;
       };
 
     Chatik::Host hostServer(true);
-    hostServer.SetOnDataReceivedCallback(onDataReceivedCallback);
+    hostServer.SetOnDataReceivedCallback(onServerDataReceivedCallback);
 
     hostServer.StartListen();
 
@@ -60,15 +56,76 @@ SUITE(HostTest)
     WAIT_FOR(hostServer.IsListening(), 200);
     WAIT_FOR(hostClient.IsListening(), 200);
 
-    const Chatik::SocketAddress address(
+    const Chatik::SocketAddress serverAddress(
       Chatik::SocketUtil::CreateIPv4FromString("127.0.0.1:5005"));
 
+    constexpr char expectedData[] = "Hello, server!";
+
     const int sentDataLen =
-      hostClient.SendData(expectedData, sizeof(expectedData), address);
+      hostClient.SendData(expectedData, sizeof(expectedData), serverAddress);
 
     CHECK_EQUAL(sizeof(expectedData), sentDataLen);
 
-    WAIT_FOR(onDataArrived.load(), 200);
+    WAIT_FOR(onServerDataArrivedTrigger.load(), 200);
+
+    CHECK_EQUAL(0, strcmp(expectedData, actualData));
+  }
+
+  TEST(HostServerSendDataToClientTest)
+  {
+    char actualData[100] = {};
+
+    std::atomic_bool onServerDataArrivedTrigger(false);
+    std::atomic_bool onClientDataArrivedTrigger(false);
+
+    auto onServerDataReceivedCallback =
+      [&](const char* data,
+          int dataLen,
+          const Chatik::SocketAddress& fromAddress) {
+        assert(data && dataLen > 0);
+        memcpy(actualData, data, dataLen);
+        onServerDataArrivedTrigger = true;
+      };
+
+    auto onClientDataReceivedCallback =
+      [&](const char* data,
+          int dataLen,
+          const Chatik::SocketAddress& fromAddress) {
+        assert(data && dataLen > 0);
+        memcpy(actualData, data, dataLen);
+        onClientDataArrivedTrigger = true;
+      };
+
+    Chatik::Host hostServer(true);
+    hostServer.SetOnDataReceivedCallback(onServerDataReceivedCallback);
+    hostServer.StartListen();
+
+    Chatik::Host hostClient(false);
+    hostClient.SetOnDataReceivedCallback(onClientDataReceivedCallback);
+    hostClient.StartListen();
+
+    WAIT_FOR(hostServer.IsListening(), 200);
+    WAIT_FOR(hostClient.IsListening(), 200);
+
+    const Chatik::SocketAddress serverAddress(
+      Chatik::SocketUtil::CreateIPv4FromString("127.0.0.1:5005"));
+    const Chatik::SocketAddress clientAddress(
+      Chatik::SocketUtil::CreateIPv4FromString("127.0.0.1:5006"));
+
+    constexpr char clientData[] = "Hello, server!";
+    int sentDataLen =
+      hostClient.SendData(clientData, sizeof(clientData), serverAddress);
+
+    WAIT_FOR(onServerDataArrivedTrigger.load(), 200);
+
+    CHECK_EQUAL(0, strcmp(clientData, actualData));
+
+    constexpr char expectedData[] = "Hello, client!";
+
+    sentDataLen =
+      hostServer.SendData(expectedData, sizeof(expectedData), serverAddress);
+
+    WAIT_FOR(onClientDataArrivedTrigger.load(), 200);
 
     CHECK_EQUAL(0, strcmp(expectedData, actualData));
   }
